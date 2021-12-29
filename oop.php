643181a63d5925e951cf1eae9f975e4b14c108a8
Fakhri17 <fakhricoffe@gmail.com>
2021-12-29 15:11:36 +0700

    add new code oop

--- oop.php
+++ oop.php
@@ -1,4 +1,6 @@
 <?php
+
+
   class person{
     public $name;
     function set_name($new_name){
@@ -9,8 +11,40 @@
     }
   }
   
+ 
+
+  class math{
+
+    private $math1;
+    private $math2;
+    
+    public function __construct($var1, $var2){
+      $this->math1 = $var1;
+      $this->math2 = $var2;
+      echo "<br>".($this->math1 + $this->math2);
+     
+    }
+
+    public function totalNumber(){
+      return $this->math1 + $this->math2;
+    }
+
+    public function multipyNumber(){
+      return $this->math1 * $this->math2;
+    }
+  }
+
   $name = new person();
   $name->set_name("Fakhri Alauddin");
   echo $name->nama = 'Fakhri';
-  echo " \nMy name is ".get_name();
+  echo "<br>".$name->get_name();
+
+
+  $defination = new math(12, 13);
+
+  $output1 = $defination->totalNumber();
+  echo "<br>".$output1;
+
+  $output2 = $defination->multipyNumber();
+  echo "<br>".$output2;
 ?>
